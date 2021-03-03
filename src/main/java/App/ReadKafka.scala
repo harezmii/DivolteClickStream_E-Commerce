6d602a7fc110599754c5acd231f01df67f886e65
harezmii <node.red.harezmi@gmail.com>
2021-03-03 21:22:13 +0300

    avro formatında gelen veri kafka dan okundu ve consolda test edildi

--- src/main/java/App/ReadKafka.scala
+++ src/main/java/App/ReadKafka.scala
@@ -1,13 +1,35 @@
 package App
 
-import org.apache.avro.SchemaBuilder
+import org.apache.avro.{Schema, SchemaBuilder}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.avro._
 import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
 import org.apache.spark.sql.functions._
 
+import java.nio.file.{Files, Paths}
+import scala.io.Source._
 object ReadKafka {
   def main(args: Array[String]): Unit = {
+
+    val schema = SchemaBuilder
+      .record("MyEventRecord")
+      .namespace("io")
+      .fields()
+      .name("timestamp").`type`().longType().noDefault()
+      .name("remoteHost").`type`().stringType().noDefault()
+      .name("eventType").`type`().nullable().stringType().stringDefault("null")
+      .name("location").`type`().nullable().stringType().stringDefault("null")
+      .name("localPath").`type`().nullable().stringType().stringDefault("null")
+      .name("type").`type`().nullable().stringType().stringDefault("null")
+      .name("title").`type`().nullable().stringType().stringDefault("null")
+      .name("price").`type`().nullable().stringType().stringDefault("null")
+      .endRecord()
+
+    val jsonSchema = new String(
+      Files.readAllBytes(Paths.get("/home/harezmi/Desktop/divolte-collector-0.9.0/conf/MyEventRecord.avsc")))
+
+
+
     val sparkSession = SparkSession
       .builder()
       .appName("read kafka avro format")
@@ -17,25 +39,17 @@ object ReadKafka {
     val readKafkaData = sparkSession
       .readStream
       .format("kafka")
-      .option("kafka.bootstrap.servers","34.123.74.41:9092")
+      .option("kafka.bootstrap.servers","34.72.107.157:9092")
       .option("subscribe","divolte")
       .load()
 
-    val castData = readKafkaData.select(
-      from_avro(col("key"), SchemaBuilder.builder().stringType().toString()).as("key"),
-      from_avro(col("value"), SchemaBuilder.builder().stringType().toString()).as("value"))
+    val castData = readKafkaData
+      .select(from_avro(col("value"),jsonSchema).as("select"))
+      .select("select.*")
 
-//    castData
-//      .select(
-//        to_avro(col("key")).as("key"),
-//        to_avro(col("value")).as("value"))
-//      .writeStream
-//      .format("console")
-//      .outputMode("append")
-//      .start()
-//      .awaitTermination()
 
-    castData.select("value")
+
+    castData
       .writeStream
       .outputMode("append")
       .format("console")
